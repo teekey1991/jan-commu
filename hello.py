3b4304f478647e7cc0490c0236e8c4d9a0ed8d0a
teekey1991 <matsunaetaiki@gmail.com>
2022-07-09 09:33:05 +0900

    address_search追加

--- hello.py
+++ hello.py
@@ -6,13 +6,23 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html")
+       return render_template("index.html")
 
     # もしPOSTリクエストだった場合はindex.htmlでmessage変数を使う
     if request.method == "POST":
         message = "講義なう"
         return render_template("index.html", message=message)
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+       return render_template("index.html")
+
+    # もしPOSTリクエストだった場合はindex.htmlでmessage変数を使う
+    if request.method == "POST":
+        message = "講義なう"
+        return render_template("index.html", message=message)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
